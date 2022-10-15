3e936dad2ed9e9d04f13a8fb0ca84af23ad260b8
Andrey <adenisov.pro@gmail.com>
2022-10-15 08:30:52 +0300

    update 15.10.2022 8-30

--- selenium_course/stepik_auto_test3.6/conftest.py
+++ selenium_course/stepik_auto_test3.6/conftest.py
@@ -2,15 +2,48 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-language = "es"
-options.add_experimental_option('prefs', {'intl.accept_languages': language})
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name',
+                     action='store',
+                     default='chrome',
+                     help="Choose browser: chrome"
+                     )
+    parser.addoption('--user_language',
+                     action='store',
+                     default=None,
+                     help="Choose from langs: en/ru/es/...)"
+                     )
+
 
 @pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("user_language")
 
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(options=options)
-    yield browser
+    browser = None
+    if browser_name == "chrome" and user_language == "es":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome")
+    yield browser, user_language
     print("\nquit browser..")
     browser.quit()
+
+
+
+
+
+
+
+
+# @pytest.fixture(scope="function")
+# def browser():
+#     print("\nstart browser for test..")
+#     browser = webdriver.Chrome()
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
--- selenium_course/stepik_auto_test3.6/test_items.py
+++ selenium_course/stepik_auto_test3.6/test_items.py
@@ -1,31 +1,21 @@
 # Путь для запуска:
 # pytest --language=es test_items.py
-# pytest --language=es stepik_auto_test3.6/test_items.py
+# pytest --language=es selenium_course/stepik_auto_test3.6/test_items.py
+# pytest -s -v selenium_course/stepik_auto_test3.6/test_items.py
 
-import pytest
-from selenium import webdriver
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.chrome.options import Options
-
-# options = Options()
-# language = "es"
-# options.add_experimental_option('prefs', {'intl.accept_languages': language})
-#
-# @pytest.fixture(scope="function")
-#
-# def browser():
-#     print("\nstart browser for test..")
-#     browser = webdriver.Chrome(options=options)
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
 
 
-def test_presence_add_cart_button(browser):
+
+def test_launching_autotests_for_different_languages(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    # link = f"https://selenium1py.pythonanywhere.com/es/catalogue/coders-at-work_207/"
     browser.get(link)
+    time.sleep(30)
 
     wait = WebDriverWait(browser, 5)
     button_text = wait.until(
@@ -36,3 +26,4 @@ def test_presence_add_cart_button(browser):
     ).text
 
     assert button_text == "Añadir al carrito", "Страница товара на сайте содержит кнопку добавления в корзину"
+
